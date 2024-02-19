a0a481a7824e07681be2a3ac324e0e1a0255fb25
Vezaiy <68307825+Vezaiy@users.noreply.github.com>
2024-02-19 01:55:59 -0800

    Delete static directory

--- static/script.js
+++
@@ -1,9 +0,0 @@
-function TextArea(){
-   document.querySelector("ck-editor").style.visibility = "visible";
-}
-
-function ChangeColor(event){
-  var text = document.getElementById("BlogArea").value;
-  text.style.color = event.target.value;
-}
-
